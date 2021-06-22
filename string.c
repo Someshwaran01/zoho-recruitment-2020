160aa0eda2c8645627893a6cd7efdec2992225f5
Someshwaran01 <86039479+Someshwaran01@users.noreply.github.com>
2021-06-22 10:34:54 +0530

    Update string.c

--- string.c
+++ string.c
@@ -1,29 +1,17 @@
 #include<stdio.h>
 int main()
 {
-  // variable
+
   char str[20];
   int len,i=1;
 
-  // take input
   printf("Enter a string: ");
   scanf("%[^\n]",str);
 
-  // count number of characters
+  
   for (len = 0; str[len]!='\0'; len++);
 
-  // for first half
-  /*for(int i=0; i<len; i++)
-  {
-    for(int j=0; j<=i; j++)
-    {
-      printf("%c", str[j]);
-    }
-
-    printf("\n"); // new line
-  }*/
-
-  // for second half
+  /
   for(int i=1; i<len; i++)
   {
     for(int j=i; j<len; j++)
